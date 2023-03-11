aa0c657c95a7d170e92bac7626242cbd4d327477
DanielaBallesteros21 <daniela.ferballesteros98@gmail.com>
2023-03-11 18:39:27 -0500

    Funciones

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 addEventListener("DOMContentLoaded", () => {
-    
+  let correo = document.querySelector(".correo")
+  let btnEnviar = document.querySelector(".btnEnviar")
+  let textoResultado = document.querySelector(".textoResultado")
+  
+  btnEnviar.addEventListener("click", () => {
+    let correoValor = correo.value
+    if(verificar(correoValor)){
+      textoResultado.textContent = correoValor
+    }else{
+      textoResultado.textContent = "correo no valido"
+    }
+  })
 });
+
+const verificar = caracter => {
+  let valor = false
+  if(caracter.includes("@")){
+    let caracterSpliteado = caracter.split("@")
+    if(caracterSpliteado[1] === "dbz.com"){
+      valor = true
+    }
+  }
+
+  return valor
+}
